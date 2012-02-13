3934389cfccdcd250d0f6c3365cafe28b7f0c106
hammale <alex@hammhome.net>
2012-02-12 22:29:29 -0600

    Started IO crap

--- NerdPig/src/me/hammale/pig/pig.java
+++ NerdPig/src/me/hammale/pig/pig.java
@@ -1,17 +1,15 @@
 package me.hammale.pig;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.io.PrintWriter;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -45,11 +43,35 @@ public class pig extends JavaPlugin {
 	        System.out.println("[NerdPig] MobDisguise isn't loaded.");
 	        pm.disablePlugin(this);
 	    }
+	    createFolders();
 	    fixNpc();
 	}
 
+	public void createFolders() {
+		File f = new File("plugins/NerdPig");
+		if(!f.exists()){
+			f.mkdir();
+		}
+		File f1 = new File("plugins/NerdPig/npcs");
+		if(!f1.exists()){
+			f1.mkdir();
+		}
+	}
+
 	public void fixNpc(){
-		readData(s);
+		for (World world : getServer().getWorlds()) {
+			for(Entity e : world.getEntities()){
+				if(e instanceof LivingEntity){
+					LivingEntity le = (LivingEntity) e;
+					if(le instanceof Player){
+						Player p = (Player) le;
+						if(CitizensManager.isNPC(e)){
+							readData(p.getName());
+						}
+					}
+				}
+			}
+		}	
 	}
 	
 	@Override
@@ -83,7 +105,7 @@ public class pig extends JavaPlugin {
 	  }
 	  
 	  public void removeFile(String s) {
-		  File f = new File("plugins/NerdPig/npcs/" + s + ".dat");
+		  File f = new File(s + ".dat");
 		  if(f.exists()){
 			  boolean success = f.delete();
 			  if (!success){
@@ -93,35 +115,48 @@ public class pig extends JavaPlugin {
 	  }
 	  
 	  public void addData(String npc, String mob) {
+		  createFolders();
+//		  try{
+//			  File file = new File("plugins/NerdPigs/npcs/" + npc + ".dat");
+//		      String str = null;		    
+//		      //if (file.exists()) {
+//		    	//  removeFile(npc);
+//		      //}
+//		      str = (npc + "," + mob);	              
+//		      PrintWriter out = new PrintWriter(new FileWriter(file, true));	    
+//		      out.println(str);
+//		      out.close();
+//		  }catch (Exception e){
+//			  System.err.println("[NerdPig] Error1: " + e.getMessage());
+//		  }		  
 		  try{
-		  File file = new File("plugins/NerdPigs/npcs/" + npc + ".dat");
-		          String str = null;		    
-		          if (file.exists()) {
-		        	  removeFile(npc);
-		          }
-		          str = (npc + "," + mob);	              
-		          PrintWriter out = new PrintWriter(new FileWriter(file, true));	    
-		          out.println(str);
-		          out.close();
-		  }catch (Exception e){
-			  System.err.println("[NerdPig] Error: " + e.getMessage());
-		  }
+			  FileWriter fstream = new FileWriter(npc + ".dat");
+			  BufferedWriter out = new BufferedWriter(fstream);
+			  String str = (npc + "," + mob);	
+			  out.write(str);
+			  out.close();
+			  }catch (Exception e){
+			  System.err.println("Error1: " + e.getMessage());
+			  }
 	}
 	  
 	  public String readData(String s){
 
 		  try{
-			  FileInputStream fstream = new FileInputStream("plugins/NerdPigs/npcs/" + s + ".dat");
-			  DataInputStream in = new DataInputStream(fstream);
-			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			  String strLine;
-			  while ((strLine = br.readLine()) != null){
-				  return strLine;
+			  File file = new File(s + ".dat");
+			  if(file.exists()){				  			  
+				  FileInputStream fstream = new FileInputStream(s + ".dat");
+				  DataInputStream in = new DataInputStream(fstream);
+				  BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				  String strLine;
+				  while ((strLine = br.readLine()) != null){
+					  return strLine;
+				  }
+				  in.close();
 			  }
-			  in.close();
 			  return null;
 		  }catch (Exception e){
-			  System.err.println("[NerdPig] Error: " + e.getMessage());
+			  System.err.println("[NerdPig] Error2: " + e.getMessage());
 		  }
 		  return null;
 	  }
